9db45f4eaddad5d2f85503c681cbf58ed9b35e8a
Wes Ehrlichman <wes@superwes.com>
2019-01-26 16:13:19 -0600

    Make level kind of work - Wes

---
+++ TidyingUp/ManCave_Level/MomTrigger.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+var original_position
+var blocking_object
+
+func _init():
+	original_position = self.position
+
+func _input_event(viewport, event, shape_idx):
+	if self.get_parent().game_over || self.get_parent().level_win:
+		pass
+	if event is InputEventMouseButton \
+    and event.button_index == BUTTON_LEFT \
+    and event.is_pressed() \
+	and self.visible:
+        self.on_click()
+
+func on_click():
+	if position != original_position:
+		position = original_position
+		blocking_object = null
+		
+func is_blocking(object):
+	return blocking_object == object
+
+func set_blocking(object):
+	blocking_object = object
