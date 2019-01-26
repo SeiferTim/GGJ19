0b36f466170cdf1d5fc416796fb1537330be41f5
SeiferTim <seifertim@gmail.com>
2019-01-26 11:37:48 -0600

    Level 01 almost done!!

--- TidyingUp/DoorClosed.gd
+++ TidyingUp/DoorClosed.gd
@@ -8,15 +8,9 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
 func _input(event):
 	if Input.is_action_just_pressed("left_click") && self.visible:
 		self.visible = false
-		self.get_parent().get_node("TrashcanLid2").visible = true
-		self.get_parent().get_node('TrashbagOpen').visible = true
+		self.get_parent().get_node("DoorOpen").visible = true
+		self.get_parent().get_node("the-full-kondo").visible = true
 		
