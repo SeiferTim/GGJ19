223e84b75351958df1aa271e0506fc1e2e043ac3
SeiferTim <seifertim@gmail.com>
2019-01-26 10:47:33 -0600

    no message

---
+++ TidyingUp/TrashcanLid.gd
@@ -0,0 +1,20 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func _input(event):
+	if Input.is_action_just_pressed("left_click") && self.visible:
+		self.visible = false
+		
