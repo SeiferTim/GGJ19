b293291754744ee0a97c172ef2242ee0f9513b4b
SeiferTim <seifertim@gmail.com>
2019-01-26 20:39:47 -0600

    Added Funeral Level, fixed up Team Level

--- TidyingUp/Funeral_Level/Funeral_Level.gd
+++ TidyingUp/Funeral_Level/Funeral_Level.gd
@@ -7,7 +7,7 @@ extends Sprite
 var game_over = false
 var level_win = false
 
-var next_scene = "res://Funeral_Level/Funeral_Level.tscn"
+var next_scene = "res://Team_Level/Team_Level.tscn"
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
--- TidyingUp/Team_Level/NaomiTarget.gd
+++ TidyingUp/Team_Level/NaomiTarget.gd
@@ -9,7 +9,8 @@ func _input_event(viewport, event, shape_idx):
     and event.button_index == BUTTON_LEFT \
     and event.is_pressed() \
 	and self.visible:
-        self.on_click()
+		get_tree().set_input_as_handled()
+		self.on_click()
 
 func on_click():
 	if !trashed:
