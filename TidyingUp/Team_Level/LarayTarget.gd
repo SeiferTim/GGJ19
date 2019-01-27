c48f37458b1e181d6455ae4bb293a95e7f75cf5c
Wes Ehrlichman <wes@superwes.com>
2019-01-26 20:25:59 -0600

    Get team level working - Wes

---
+++ TidyingUp/Team_Level/LarayTarget.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var trashed = false
+
+func _input_event(viewport, event, shape_idx):
+	if self.get_parent().game_over:
+		pass
+	if event is InputEventMouseButton \
+    and event.button_index == BUTTON_LEFT \
+    and event.is_pressed() \
+	and self.visible:
+        self.on_click()
+
+func on_click():
+	if !trashed:
+		self.position = Vector2(807, 400)
+		self.rotate(deg2rad(180))
+		self.get_parent().move_child(self, 0)
+		trashed = true
---
+++ TidyingUp/Team_Level/Team_Level.gd
@@ -0,0 +1,17 @@
+extends Sprite
+
+var game_over = false
+var level_win = false
+
+var next_scene = "res://Trash_Level/Trash_Level.tscn"
+
+func _process(delta):
+	if !game_over && !level_win:
+		if (get_node("TimTarget").trashed
+		 && get_node("NaomiTarget").trashed
+		 && get_node("MichaelTarget").trashed
+		 && get_node("LarayTarget").trashed
+		 && get_node("WesTarget").trashed
+		 && get_node("JevionTarget").trashed
+		 ):
+				level_win = true
