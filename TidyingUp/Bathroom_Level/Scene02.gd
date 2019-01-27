e4644a85a9b277af4ca6d863be7459261428c0fa
Wes Ehrlichman <wes@superwes.com>
2019-01-26 22:10:40 -0600

    Fix(ish) man cave level - Wes

--- TidyingUp/Bathroom_Level/Scene02.gd
+++ TidyingUp/Bathroom_Level/Scene02.gd
@@ -8,14 +8,3 @@ var game_over = false
 var level_win = false
 
 var next_scene = "res://Basement_Level/Basement_Level.tscn"
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	
-	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
--- TidyingUp/ManCave_Level/XboxControllerTrigger.gd
+++ TidyingUp/ManCave_Level/XboxControllerTrigger.gd
@@ -1,20 +1,31 @@
 extends Area2D
 
+var has_been_blocked = false
+
 func _input_event(viewport, event, shape_idx):
 	if self.get_parent().game_over || self.get_parent().level_win:
 		return
 	if event is InputEventMouseButton \
     and event.button_index == BUTTON_LEFT \
     and event.is_pressed() \
-	and !get_tree().is_input_handled() \
 	and self.visible:
-		get_tree().set_input_as_handled()
 		self.on_click()
 
 func on_click():
 	var mom = self.get_parent().get_node("MomTrigger")
-	if mom.is_blocking(self) && mom.is_home():
-		self.visible = false
-		mom.is_blocking(null)
-	if !mom.is_blocking(self):
-		mom.set_blocking(self)
+	if has_been_blocked:
+		if mom.position != self.position:
+			self.visible = false
+			mom.is_blocking(null)
+		else:
+			send_mom_home()
+	else:
+		if !mom.is_blocking(self):
+			mom.set_blocking(self)
+			mom.position = self.position
+			has_been_blocked = true
+
+
+func send_mom_home():
+	var mom = self.get_parent().get_node("MomTrigger")
+	mom.position = mom.original_position
